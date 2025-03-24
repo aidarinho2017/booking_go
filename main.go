cfc4dbc735a72b1d46175e5385d47e0a3274e7f2
aidarinho <isakhanovaidar@gmail.com>
2025-03-24 23:04:26 +0500

    added endpoints

--- main.go
+++ main.go
@@ -2,125 +2,120 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-var remainingTickets uint = 50
-var bookings = make([]UserData, 0)
-
-const conferenceName = "Go Conference"
-
-type UserData struct {
-	firstName       string
-	lastName        string
-	email           string
-	numberOfTickets uint
+type User struct {
+	ID    int
+	Name  string
+	Email string
 }
 
-var wg = sync.WaitGroup{}
+func (u User) GetDetails() string {
+	return fmt.Sprintf("ID: %d, Name: %s, Email: %s", u.ID, u.Name, u.Email)
+}
+
+type Student struct {
+	User
+	GPA   float64
+	Marks map[string]float64
+}
+
+type Master struct {
+	Student
+	ResearchTopic string
+}
+
+type Teacher struct {
+	User
+	Subject string
+}
+
+func (s Student) GetDetails() string {
+	return fmt.Sprintf("ID: %d, Name: %s, Email: %s", s.ID, s.Name, s.Email)
+}
+
+func (s Student) GetMarks() {
+	for mark, grade := range s.Marks {
+		fmt.Println(mark, grade)
+	}
+}
+
+func (s Student) GetGPA() float64 {
+	var GPA float64 = 0
+	var count float64 = 0
+	for _, grade := range s.Marks {
+		count++
+		GPA += grade
+	}
+	return GPA / count
+}
+
+func (t Teacher) GetDetails() string {
+	return fmt.Sprintf("Id: %d, Name: %s", t.ID, t.Name)
+}
+
+func (t Teacher) mark(s *Student, subject string, mark float64) {
+	if s.Marks == nil {
+		s.Marks = make(map[string]float64)
+	}
+	s.Marks[subject] = mark
+}
+
+var students = map[int]Student{
+	1: {User: User{ID: 1, Name: "Alice", Email: "alice@example.com"}, GPA: 3.5, Marks: map[string]float64{"Math": 85}},
+	2: {User: User{ID: 2, Name: "Bob", Email: "bob@example.com"}, GPA: 3.8, Marks: map[string]float64{"Physics": 90}},
+}
+
+func AddStudent(s *Student) map[int]Student {
+	students[s.ID] = *s
+	return students
+}
+func GetStudents() map[int]Student {
+	return students
+}
+
+func DeleteStudent(id int) {
+	if students[id].Marks != nil {
+		delete(students[id].Marks, "Math")
+	}
+}
+
+//
+//func GetStudents(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("Content-Type", "application/json")
+//	json.NewEncoder(w).Encode(students)
+//}
 
 func main() {
-	/*greetUsers()
 
-	for {
-		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+	//student := Student{
+	//	User:  User{ID: 1, Name: "Alice", Email: "alice@example.com"},
+	//	GPA:   3.67,
+	//	Marks: make(map[string]float64),
+	//}
+	//
+	//AddStudent(&student)
+	//fmt.Println(GetStudents())
+	//
+	//teacher := Teacher{
+	//	User:    User{ID: 1, Name: "Alice", Email: "alice@example.com"},
+	//	Subject: "Math",
+	//}
+	//
+	//teacher.mark(&student, "Literature", 5.45)
+	//teacher.mark(&student, "Literature2", 1)
+	//teacher.mark(&student, "Literature3", 5.45)
+	//teacher.mark(&student, "Literature4", 5.45)
+	//fmt.Println(student.GetGPA())
+	//
+	//student.GetMarks()
 
-		if isValidName && isValidEmail && isValidTicketNumber {
+	//http.HandleFunc("/students", GetStudents)
+	//http.ListenAndServe(":8080", nil)
 
-			bookTicket(userTickets, firstName, lastName, email)
+	var s = [3]string{"aa", "bb", "cc"}
 
-			wg.Add(1)
-			go sendTicket(userTickets, firstName, lastName, email)
-
-			firstNames := getFirstNames()
-			fmt.Printf("The first names of bookings are: %v\n", firstNames)
-
-			if remainingTickets == 0 {
-				// end program
-				fmt.Println("Our conference is booked out. Come back next year.")
-				// break
-			}
-		} else {
-			if !isValidName {
-				fmt.Println("first name or last name you entered is too short")
-			}
-			if !isValidEmail {
-				fmt.Println("email address you entered doesn't contain @ sign")
-			}
-			if !isValidTicketNumber {
-				fmt.Println("number of tickets you entered is invalid")
-			}
-		}
+	for i, w := range s {
+		fmt.Println(i, w)
 	}
-	wg.Wait()
-	*/
-	CreateFile()
-	ReadFile()
-	//RemoveFile()
 }
-func greetUsers() {
-	fmt.Printf("Welcome to our booking application\n")
-	fmt.Printf("We have total of tickets and are still available.\n")
-	fmt.Println("Get your tickets here to attend")
-	fmt.Println("Welcome to Booking app")
-}
-
-func getUserInput() (string, string, string, uint) {
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
-
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
-
-	fmt.Println("Enter number of tickets: ")
-	fmt.Scan(&userTickets)
-
-	return firstName, lastName, email, userTickets
-}
-
-func bookTicket(userTickets uint, firstName string, lastName string, email string) {
-	remainingTickets = remainingTickets - userTickets
-
-	var userData = UserData{
-		firstName:       firstName,
-		lastName:        lastName,
-		email:           email,
-		numberOfTickets: userTickets,
-	}
-
-	bookings = append(bookings, userData)
-	fmt.Printf("List of bookings is %v\n", bookings)
-
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-}
-
-func getFirstNames() []string {
-	firstNames := make([]string, len(bookings))
-	for i := 0; i < len(bookings); i++ {
-		firstNames[i] = bookings[i].firstName
-	}
-	return firstNames
-}
-
-func sendTicket(userTickets uint, firstName string, lastName string, email string) {
-	time.Sleep(50 * time.Second)
-	var ticket = fmt.Sprintf("%v tickets for %v %v", userTickets, firstName, lastName)
-	fmt.Println("#################")
-	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
-	fmt.Println("#################")
-	wg.Done()
-}
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
